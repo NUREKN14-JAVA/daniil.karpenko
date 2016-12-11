a8b55cce94d94b94f5bb7fb6258831c0f6c073c7
DaniilKarpenko <daniil.karpenko@nure.ua>
2016-12-11 20:55:51 +0200

    lab4 commit

---
+++ usermanagement/src/main/java/com/daniilkarpenko/usermanagement/web/AddServlet.java
@@ -0,0 +1,23 @@
+package com.daniilkarpenko.usermanagement.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.daniilkarpenko.usermanagement.User;
+import com.daniilkarpenko.usermanagement.db.DaoFactory;
+import com.daniilkarpenko.usermanagement.db.DatabaseException;
+
+public class AddServlet extends EditServlet {
+
+	protected void processUser(User user) throws DatabaseException {
+        DaoFactory.getInstance().getUserDao().create(user);
+    }
+	
+	protected void showPage(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/add.jsp").forward(req, resp);
+    }
+}
